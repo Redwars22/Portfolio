a9f46ed6cebd7f901895448449c8af62148029cb
Redwars22 <andrewpereira.megabyte@gmail.com>
2021-10-27 22:54:35 -0300

    Adicionado Versão ALPHA do Project Eris

---
+++ app/Eris/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>📑️🌟️ PROJECT ERIS<</title>
+    <script src="dataHandler.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+      <h1>📑️🌟️ PROJECT ERIS</h1>
+    </header>
+    <main>
+        <form action="handler.php" method="POST">
+            <textarea name="" id="" cols="90" rows="13"></textarea>
+            <br>
+            <button class="button download" type="submit">BAIXAR COMO RTF</button>
+        </form>
+        <div class="container">
+          <button class="button save" onClick="">SALVAR</button>
+          <button class="button open" onClick="">ABRIR</button>
+          <button class="button delete" onClick="">DELETAR</button>
+        </div>
+    </main>
+</body>
+</html>
